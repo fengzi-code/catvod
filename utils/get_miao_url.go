585a641199b2546372b3271769b87f633563631e
235546@qq.com <235546@qq.com>
2022-05-04 16:28:55 +0800

    修复zjmiao解析接口不稳定的情况

--- utils/get_miao_url.go
+++ utils/get_miao_url.go
@@ -1,12 +1,10 @@
 package utils
 
 import (
-	"catvod/global"
 	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"log"
-	"net/http"
 	"time"
 )
 
@@ -14,28 +12,17 @@ type Player struct {
 	Url string `json:"url"`
 }
 
-var cookies []*http.Cookie
-
 func GetMiaoUrl(url string) (playerUrl string) {
-	headers := global.Headers
-	headers["origin"] = "zjmiao.com"
 	client := resty.New()
-	client.SetRetryWaitTime(time.Second * 15) //设置超时时间
-	get, _ := client.R().
-		SetHeaders(headers).
-		SetCookies(cookies).
-		Get(url)
+	client.SetRetryWaitTime(time.Second * 5) //设置超时时间
+	get, _ := client.R().Get(url)
 	fmt.Printf("请求地址: %s, 请求状态码: %d, 请求结果: %+v\n", url, get.StatusCode(), get.Result())
-	cookies = get.Cookies()
-	fmt.Println(cookies)
 	body := string(get.Body())
 	player := GetBetweenStr(body, `player_aaaa=`, `<`)
-	fmt.Println("354444444444", player)
 	var playerJson Player
 	err := json.Unmarshal([]byte(player), &playerJson)
 	if err != nil {
 		log.Println("json unmarshal error:", err)
-		return
 	}
 	fmt.Println(playerJson.Url)
 	return playerJson.Url
--- utils/zjmiao_jx.go
+++ utils/zjmiao_jx.go
@@ -8,11 +8,9 @@ import (
 )
 
 func GetZJMiaoUrl(reqUrl string) (playUrl string) {
-
 	headers := global.Headers
 	headers["Host"] = "jx.zjmiao.com"
 	headers["origin"] = "jx.zjmiao.com"
-
 	client := resty.New()
 	client.SetRetryWaitTime(time.Second * 15) //设置超时时间
 	get, _ := client.R().
@@ -20,12 +18,9 @@ func GetZJMiaoUrl(reqUrl string) (playUrl string) {
 		Get(reqUrl)
 	fmt.Printf("请求地址: %s, 请求状态码: %d, 请求结果: %+v\n", reqUrl, get.StatusCode(), get.Result())
 	body := string(get.Body())
-	fmt.Println(body)
-	//fmt.Println(body)
 	btToken := GetBetweenStr(body, `bt_token = "`, `"`)
 	url := GetBetweenStr(body, `getVideoInfo("`, `"`)
 	aesPass := "63f49d21a0dccf3c"
-
 	playUrl, _ = Decrypt(url, aesPass, btToken)
 	return playUrl
 }
