3bc9f1ea8d81f020059f2f29e074e0107492c471
235546@qq.com <235546@qq.com>
2022-09-01 10:32:28 +0800

    type1 youku 完善搜索功能

--- service/youku/search.go
+++ service/youku/search.go
@@ -5,21 +5,18 @@ import (
 	"catvod/model/youku/response"
 	"catvod/utils"
 	"crypto/md5"
-	"encoding/json"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 	"time"
 )
 
-const searchUrl = "https://so.youku.com/search_video/q_%s"
 const (
 	sApi      = `mtop.youku.soku.yksearch`
 	sAppkey   = `23774304`
-	sData     = `{"searchType":1,"keyword":"女人","pg":1,"pz":10,"site":1,"appCaller":"pc","appScene":"mobile_multi","userTerminal":2,"sdkver":313,"userFrom":1,"noqc":0,"aaid":"bf533a34a7532e5d1d0893b9e2e3bdef","ftype":0,"duration":"","categories":"","ob":"","utdId":"tP/hF4OrRWkCAXL6lgpWdwlE","userType":"guest","userNumId":0,"searchFrom":"1","sourceFrom":"home"}`
+	sData     = `{"searchType":1,"keyword":"SoKeyword","pg":1,"pz":10,"site":1,"appCaller":"pc","appScene":"mobile_multi","userTerminal":2,"sdkver":313,"userFrom":1,"noqc":0,"aaid":"bf533a34a7532e5d1d0893b9e2e3bdef","ftype":0,"duration":"","categories":"","ob":"","utdId":"tP/hF4OrRWkCAXL6lgpWdwlE","userType":"guest","userNumId":0,"searchFrom":"1","sourceFrom":"home"}`
 	searcapi1 = "https://acs.youku.com/h5/mtop.youku.soku.yksearch/2.0/?jsv=2.5.1&appKey=" + sAppkey
 	sH5Url    = "https://acs.youku.com/h5/" + sApi + "/2.0/?jsv=2.5.1&appKey=" + "23774304" + "&api=" + sApi
 )
@@ -27,10 +24,9 @@ const (
 var cookies []*http.Cookie
 
 func (this *YOUKU) Search(wd string) (res []model.VodInfo) {
-
 	client := resty.New()
 	client.SetRetryWaitTime(time.Second * 15) //设置超时时间
-
+	data := strings.Replace(sData, `SoKeyword`, wd, 1)
 	fmt.Println(sH5Url)
 	get, _ := client.R().
 		SetHeaders(
@@ -54,15 +50,12 @@ func (this *YOUKU) Search(wd string) (res []model.VodInfo) {
 	timeStamp := tokens[1]
 	token := tokens[0]
 	fmt.Println(token, timeStamp, sAppkey)
-
-	sign := token + "&" + timeStamp + "&" + sAppkey + "&" + sData
+	sign := token + "&" + timeStamp + "&" + sAppkey + "&" + data
 	//md5加密
 	signByte := md5.Sum([]byte(sign))
 	sign = fmt.Sprintf("%x", signByte)
-
-	api := searcapi1 + `&t=` + timeStamp + `&sign=` + sign + "&api=" + sApi + `&type=originaljson&v=2.0&ecode=1&dataType=json&jsonpIncPrefix=headerSearch&data=` + url.QueryEscape(sData)
+	api := searcapi1 + `&t=` + timeStamp + `&sign=` + sign + "&api=" + sApi + `&type=originaljson&v=2.0&ecode=1&dataType=json&jsonpIncPrefix=headerSearch&data=` + url.QueryEscape(data)
 	fmt.Println(api)
-	var youkuSearch response.YoukuSearch
 	get, _ = client.R().
 		SetHeaders(
 			map[string]string{
@@ -76,81 +69,33 @@ func (this *YOUKU) Search(wd string) (res []model.VodInfo) {
 		Get(api)
 
 	c := get.Result().(*response.YoukuSearch)
-
-	//JSON.data.nodes[0].nodes[0].nodes[0].data.tempTitle
-	//JSON.data.nodes[1].nodes[0].nodes[0].data.tempTitle
-	//JSON.data.nodes[1].nodes[1].data.tempTitle
-	//JSON.data.nodes[4].nodes[0].nodes[0].data.tempTitle
-	//JSON.data.nodes[4].nodes[1].data.tempTitle
-	//JSON.data.nodes[1].nodes[1].nodes[0].data.videoId
-	//JSON.data.nodes[0].nodes[0].nodes[0].data.realShowId
-	//JSON.data.nodes[1].nodes[0].nodes[0].data.posterDTO.vThumbUrl
-	//https://v.youku.com/v_nextstage/id_6c0ba80675594b04a047.html
-	//JSON.data.nodes[1].nodes[0].nodes[0].data.stripeBottom
-	fmt.Println(55555555555, len(c.Data.Nodes))
 	for _, node := range c.Data.Nodes {
 		for _, s := range node.Nodes {
 
 			if s.Nodes[0].Data.TempTitle != "" && s.Nodes[0].Data.IsYouku == 1 {
 				name := s.Nodes[0].Data.TempTitle
 				showid := s.Nodes[0].Data.RealShowId
-				resp, err := http.Get("https://v.youku.com/v_nextstage/id_" + showid + ".html")
+				resp, err := http.Get("https://v.youku.com/v_nextstage/id_" + showid + ".html?spm=a2h0c.8166622.PhoneSokuProgram_1.dposter")
 				request := resp.Request.URL.String()
 				if err != nil {
 					fmt.Println(err)
 					request = ""
 				}
-
 				pic := s.Nodes[0].Data.PosterDTO.VThumbUrl
 				remark := s.Nodes[0].Data.StripeBottom
-				fmt.Println(name, showid, pic, remark, request)
-			}
-		}
-
-	}
-	os.Exit(0)
-
-	surl := fmt.Sprintf(searchUrl, url.QueryEscape(wd))
-	client = resty.New()
-	fmt.Println("cookies:  ", cookies)
-	client.SetRetryWaitTime(time.Second * 15) //设置超时时间
-	get, _ = client.R().
-		SetHeaders(
-			map[string]string{
-				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-				"Referer":    surl,
-				"Origin":     "https://v.youku.com",
-			},
-		).
-		SetCookies(cookies).
-		Get(surl)
-	cookies = get.Cookies()
-	body := string(get.Body())
-
-	bodyJson := utils.GetBetweenStr(body, `__INITIAL_DATA__ =`, `;window`)
-	fmt.Println(bodyJson)
-	bodyJson = utils.Unicode2utf8(bodyJson)
-	//var youkuSearch response.YoukuSearch1
-	err := json.Unmarshal([]byte(bodyJson), &youkuSearch)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, node := range youkuSearch.Data.Nodes {
-		for _, nod := range node.Nodes {
-			if nod.Data.FeatureDTO.Text != "" && nod.Nodes[0].Data.VideoId != "" {
-				remark := strings.ReplaceAll(nod.Data.FeatureDTO.Text, `<em>`, ``)
-				remark = strings.ReplaceAll(remark, `</em>`, ``)
+				vid := utils.GetBetweenStr(request, `id_`, `.html`)
+				fmt.Println(name, showid, pic, remark, vid)
 				res = append(
 					res, model.VodInfo{
-						VodId:      nod.Nodes[0].Data.VideoId,
-						VodName:    nod.Data.TempTitle,
-						VodPic:     nod.Data.PosterDTO.VThumbUrl,
+						VodId:      vid,
+						VodName:    name,
+						VodPic:     pic,
 						VodRemarks: remark,
 					},
 				)
 			}
 		}
+
 	}
 	return
 }
